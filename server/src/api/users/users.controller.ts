ec8bf967eea23518a58d7a2269d6e645dccb8e12
Ellie-kang <kya034203@gmail.com>
2026-05-23 02:17:55 +0900

    chore: 구 server/src 잔여 파일 제거

--- server/src/api/users/users.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from 'express';
-
-import { AuthenticatedRequest } from '../../types/index.js';
-
-export async function getMe(req: AuthenticatedRequest, res: Response) {
-  res.json({ success: true, data: req.user ?? null });
-}
