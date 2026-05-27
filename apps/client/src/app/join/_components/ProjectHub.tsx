d6ba56f14bd3d6b2e7e1c6d5c919b62ac0811d56
jhlee <jhlee89898989@gmail.com>
2026-05-27 13:21:18 +0900

    feat: 팀 합류 페이지 초기 UI 작업

---
+++ apps/client/src/app/join/_components/ProjectHub.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { Card, Tabs } from '@heroui/react';
+
+const TabOptions = [
+  {
+    id: 'create',
+    label: '새 프로젝트 생성',
+  },
+  {
+    id: 'invite',
+    label: '초대 코드로 참여',
+  },
+];
+
+export default function ProjectHub() {
+  return (
+    <section className="mx-auto grid min-h-dvh w-full max-w-6xl items-center gap-16 px-8 py-16 md:grid-cols-[1fr_0.95fr]">
+      <Card>
+        <Card.Title>
+          <p>PROJECT HUB</p>
+          <p>협업할 프로젝트 팀 연결</p>
+          <p>
+            새로운 스터디룸을 생성하거나 초대 코드를 통해 기존 룸에 진입합니다
+          </p>
+        </Card.Title>
+        <Card.Content>
+          <Tabs>
+            <Tabs.ListContainer>
+              <Tabs.List>
+                {TabOptions.map(option => (
+                  <Tabs.Tab
+                    key={`team-selection-tabs-${option.id}`}
+                    id={option.id}
+                  >
+                    <p>{option.label}</p>
+                    <Tabs.Indicator />
+                  </Tabs.Tab>
+                ))}
+              </Tabs.List>
+            </Tabs.ListContainer>
+            {TabOptions.map(option => (
+              <Tabs.Panel
+                key={`team-selection-tabs-panel-${option.id}`}
+                id={option.id}
+              >
+                <p>{option.label}</p>
+              </Tabs.Panel>
+            ))}
+          </Tabs>
+        </Card.Content>
+      </Card>
+    </section>
+  );
+}
