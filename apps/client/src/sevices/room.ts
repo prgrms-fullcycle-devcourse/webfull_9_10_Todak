b70ca9b0c8fd8b6270e3d94d7d6182900bed0d2b
jhlee <jhlee89898989@gmail.com>
2026-05-29 09:25:28 +0900

    feat: api wrapper 함수 생성

---
+++ apps/client/src/sevices/room.ts
@@ -0,0 +1,11 @@
+export function fetchMyRooms() {}
+
+export function fetchRoomInfo() {}
+
+export function createRoom() {}
+
+export function joinRoom() {}
+
+export function updateRoomSettings() {}
+
+export function deleteRoom() {}
