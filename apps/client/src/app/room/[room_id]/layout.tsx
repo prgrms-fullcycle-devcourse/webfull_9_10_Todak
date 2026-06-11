fd3dbc9c4cd63a77f2b795d5140ed2274f672490
jhleeWEB <58125528+jhleeWEB@users.noreply.github.com>
2026-06-11 13:37:35 +0900

    feat: 메타데이터 삽입

--- apps/client/src/app/room/[room_id]/layout.tsx
+++ apps/client/src/app/room/[room_id]/layout.tsx
@@ -1,7 +1,54 @@
+import { apiServer } from '@/lib/api.server';
 import RoomMainContainer from './_components/RoomMainContainer';
 import SidebarContainer from './_components/SidebarContainer';
 
-export default function RoomLayout({
+import { Metadata } from 'next';
+import { fetchRoomInfo } from '@/services/rooms/api.server';
+
+type Props = {
+  params: Promise<{ room_id: string }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const roomID = (await params).room_id;
+  const roomInfo = await fetchRoomInfo(roomID);
+  if (!roomInfo) {
+    return {};
+  }
+  const {
+    room_name,
+    host_name,
+    repo_name,
+    member_count,
+    member_names,
+    max_members,
+    created_at,
+  } = roomInfo;
+  const title = `${room_name}에서 함께 협업을 시작하세요~! [${member_count}/${max_members}]`;
+  const description = `${host_name}님이 ${room_name}에서 ${member_names.map((n, i) => (i === member_names.length ? `${n}님과` : `${n}, `))} 협업을 기다리고 있습니다.`;
+  return {
+    title: title,
+    description: description,
+    openGraph: {
+      title: title,
+      description: description,
+      url: `${process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000'}/room/${roomID}`,
+      type: 'article',
+      publishedTime: created_at, // 생성일
+      authors: [host_name, ...member_names],
+      images: [
+        {
+          url: '/metadata/todak-room-image.webp',
+          width: 1200,
+          height: 630,
+          alt: '토닥윗미 대표 이미지',
+        },
+      ],
+    },
+  };
+}
+
+export default async function RoomLayout({
   children,
   sidebar,
   chats,
@@ -10,7 +57,6 @@ export default function RoomLayout({
   return (
     <div className="room-layout-container">
       <SidebarContainer>{sidebar}</SidebarContainer>
-
       <RoomMainContainer chats={chats}>
         {renderer ?? children}
       </RoomMainContainer>
---
+++ apps/client/src/services/rooms/api.server.ts
@@ -0,0 +1,6 @@
+import { apiServer } from '@/lib/api.server';
+import { PublicRoomInfo } from './model';
+
+export async function fetchRoomInfo(roomID: string) {
+  return apiServer.get<PublicRoomInfo>(`/rooms/${roomID}`);
+}
